df11a1a7b9db877c2252b5d524c6e141d419f57f
wappon28dev <wappon_28_dev@icloud.com>
2024-03-11 17:54:01 +0900

    🚧

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { type ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,7 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
